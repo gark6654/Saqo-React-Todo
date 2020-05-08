748c9635277d1607f1cf98e5af11d817a51bf60f
gark6654 <44678628+gark6654@users.noreply.github.com>
2020-05-08 12:59:00 +0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './tags/App';
+import './script';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
