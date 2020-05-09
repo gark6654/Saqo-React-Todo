37f89e51e10b3f05988a8c04f5e6edd19986d982
gark6654 <gark.6654@gmail.com>
2020-05-10 00:01:48 +0400

    Renamed tags folder name to components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './tags/App';
+import App from './components/App';
 import './script';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
